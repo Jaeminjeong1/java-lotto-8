baa210cb8974b91d4864fae9bf6c2e327130b1ed
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-01 21:45:57 +0900

    feat(InputView): 당첨 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,6 +8,7 @@ import static lotto.utils.Validator.validateNumberFormat;
 public class InputView {
 
     private static final String BUY_PRICE_INPUT = "구입금액을 입력해 주세요.";
+    private static final String WINNER_NUM_INPUT = "당첨 번호를 입력해 주세요.";
 
     private InputView() {
     }
@@ -22,5 +23,13 @@ public class InputView {
         validateNumberFormat(userInput);
 
         return Long.parseLong(userInput);
+    }
+
+    public String inputWinnerNum() {
+        System.out.println(WINNER_NUM_INPUT);
+        String userInput = Console.readLine();
+        validateEmptyInput(userInput);
+
+        return userInput;
     }
 }
