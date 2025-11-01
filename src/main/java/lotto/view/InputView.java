96debdf0d5da90f2f63b877de382c656653c8525
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-01 21:40:17 +0900

    feat(InputView): 로또 구입금액 입력받는 로직 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,26 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static lotto.utils.Validator.validateEmptyInput;
+import static lotto.utils.Validator.validateNumberFormat;
+
 public class InputView {
+
+    private static final String BUY_PRICE_INPUT = "구입금액을 입력해 주세요.";
 
     private InputView() {
     }
     public static InputView create() {
         return new InputView();
+    }
+
+    public long inputUserPrice() {
+        System.out.println(BUY_PRICE_INPUT);
+        String userInput = Console.readLine();
+        validateEmptyInput(userInput);
+        validateNumberFormat(userInput);
+
+        return Long.parseLong(userInput);
     }
 }
