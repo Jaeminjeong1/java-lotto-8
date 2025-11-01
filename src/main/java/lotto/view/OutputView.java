7a22bb56d1a3753c207b12b7a62d4084bff73c20
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-02 00:37:43 +0900

    feat(OutputView): 구입한 로또 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,23 @@
 package lotto.view;
 
+import lotto.dto.LottoDto;
+
+import java.util.List;
+
 public class OutputView {
 
     private OutputView() {}
     public static OutputView create() {
         return new OutputView();
+    }
+
+    public static void printLottos(List<LottoDto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+
+        for (LottoDto dto : lottos) {
+            System.out.println(dto.sorted());
+        }
+
+        System.out.println();
     }
 }
