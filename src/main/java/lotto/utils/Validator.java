60be56c75dcbb51e96a6b5f80e9a0f79ebc69969
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-01 21:38:16 +0900

    feat(Validator): 숫자 형식 검증로직 구현

--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -1,11 +1,21 @@
 package lotto.utils;
 
+import java.util.regex.Pattern;
+
 import static lotto.utils.ErrorMessage.EMPTY_INPUT_ERROR;
 
 public class Validator {
 
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
+
     public static void validateEmptyInput(String userInput) {
         if (userInput == null ||userInput.isBlank()) {
+            throw new IllegalArgumentException(EMPTY_INPUT_ERROR.getMessage());
+        }
+    }
+
+    public static void validateNumberFormat(String userInput) {
+        if (!NUMBER_PATTERN.matcher(userInput).matches()) {
             throw new IllegalArgumentException(EMPTY_INPUT_ERROR.getMessage());
         }
     }
