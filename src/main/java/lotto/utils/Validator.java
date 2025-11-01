fbb7fc06a55349664c5ac7fbde70dc46846098cd
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-01 21:28:03 +0900

    feat(validator): 빈 입력값 검증로직 구현

--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -1,4 +1,12 @@
 package lotto.utils;
 
+import static lotto.utils.ErrorMessage.EMPTY_INPUT_ERROR;
+
 public class Validator {
+
+    public static void validateEmptyInput(String userInput) {
+        if (userInput == null ||userInput.isBlank()) {
+            throw new IllegalArgumentException(EMPTY_INPUT_ERROR.getMessage());
+        }
+    }
 }
