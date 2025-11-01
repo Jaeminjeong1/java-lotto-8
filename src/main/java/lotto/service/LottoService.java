be1a77c2414279dc17e48aa46d11165fe1b73c57
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-02 01:56:36 +0900

    feat(LottoService): 로또 당첨 통계 집계 로직 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,7 +1,8 @@
 package lotto.service;
 
 import lotto.domain.Lotto;
-import lotto.domain.Price;
+import lotto.domain.LottoPrice;
+import lotto.domain.Result;
 import lotto.dto.LottoDto;
 import lotto.utils.Parser;
 import lotto.utils.RandomGenerator;
@@ -9,6 +10,8 @@ import lotto.utils.RandomGenerator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static lotto.utils.ErrorMessage.BONUS_NUM_CONTAINS_ERROR;
 
@@ -19,7 +22,7 @@ public class LottoService {
 
     public List<LottoDto> generateLottos(long userPrice) {
         List<LottoDto> result = new ArrayList<>();
-        long lottoCount = Price.from(userPrice).getLottoCount();
+        long lottoCount = LottoPrice.from(userPrice).getLottoCount();
 
         for (int i = 0; i < lottoCount; i++) {
             List<Integer> numbers = RandomGenerator.generateRandomNum();
@@ -44,5 +47,31 @@ public class LottoService {
             throw new IllegalArgumentException(BONUS_NUM_CONTAINS_ERROR.getMessage());
         }
         return bonusNumber;
+    }
+
+    public List<Result> findMatchCount(List<LottoDto> lottos, LottoDto winnerLotto, int bonusNum) {
+        return lottos.stream()
+                .map(lotto -> {
+                    long matchCount = lotto.numbers().stream()
+                            .filter(winnerLotto.numbers()::contains)
+                            .count();
+
+                    boolean bonusMatch = lotto.numbers().contains(bonusNum);
+
+                    return findResult((int) matchCount, bonusMatch);
+                })
+                .toList();
+    }
+
+    private Result findResult(int matchCount, boolean bonusMatch) {
+        return Arrays.stream(Result.values())
+                .filter(r -> r.getMatchCount() == matchCount && r.isBonusMatch() == bonusMatch)
+                .findFirst()
+                .orElse(Result.MISS);
+    }
+
+    public Map<Result, Long> summarizeResults(List<Result> results) {
+        return results.stream()
+                .collect(Collectors.groupingBy(result -> result, Collectors.counting()));
     }
 }
