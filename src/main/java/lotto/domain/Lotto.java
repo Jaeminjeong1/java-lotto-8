ed2dec89d6a62e56cc5a787a723ce1ea999bfa64
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-01 22:24:49 +0900

    refactor(Lotto): 로또 숫자 개수 상수화

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,6 +5,9 @@ import java.util.List;
 import static lotto.utils.ErrorMessage.LOTTO_NUM_COUNT_ERROR;
 
 public class Lotto {
+
+    private static final int LOTTO_NUM_COUNT = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -13,7 +16,7 @@ public class Lotto {
     }
 
     private void validateLottoCount(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUM_COUNT) {
             throw new IllegalArgumentException(LOTTO_NUM_COUNT_ERROR.getMessage());
         }
     }
