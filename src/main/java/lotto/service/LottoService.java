3a369b1f2547a7f0ef8392828b69815dd657deee
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-01 23:10:14 +0900

    feat(LottoService): 구입 금액을 가지고 로또 생성 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,11 +1,24 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+import lotto.domain.Price;
+import lotto.utils.RandomGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoService {
 
     public LottoService() {
     }
 
-    public void lottoGenerate(long userPrice) {
-        validatePrice
+    public List<Lotto> lottoGenerate(long userPrice) {
+        List<Lotto> lottos = new ArrayList<>();
+        long lottoCount = Price.from(userPrice).getLottoCount();
+
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(Lotto.from(RandomGenerator.generateRandomNum()));
+        }
+        return lottos;
     }
 }
