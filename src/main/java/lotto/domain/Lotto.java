1eb6f31cf9202731c477e2a23ea5f1a20d5af37d
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-01 22:28:34 +0900

    feat(Lotto): 로또 숫자 범위 검증 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,15 +3,19 @@ package lotto.domain;
 import java.util.List;
 
 import static lotto.utils.ErrorMessage.LOTTO_NUM_COUNT_ERROR;
+import static lotto.utils.ErrorMessage.LOTTO_NUM_RANGE_ERROR;
 
 public class Lotto {
 
     private static final int LOTTO_NUM_COUNT = 6;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateLottoCount(numbers);
+        validateNumberRange(numbers);
         this.numbers = numbers;
     }
 
@@ -19,6 +23,15 @@ public class Lotto {
         if (numbers.size() != LOTTO_NUM_COUNT) {
             throw new IllegalArgumentException(LOTTO_NUM_COUNT_ERROR.getMessage());
         }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER)
+                .findAny()
+                .ifPresent(n -> {
+                    throw new IllegalArgumentException(LOTTO_NUM_RANGE_ERROR.getMessage());
+                });
     }
 
     // TODO: 추가 기능 구현
