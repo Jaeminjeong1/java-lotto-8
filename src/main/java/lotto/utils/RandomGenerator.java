29ad93959bc876e8430a2cdbd7ee459df2e4a2ff
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-01 22:39:51 +0900

    feat(RandomGenerator): 랜덤 숫자 생성기 기능 구현

--- src/main/java/lotto/utils/RandomGenerator.java
+++ src/main/java/lotto/utils/RandomGenerator.java
@@ -1,4 +1,19 @@
 package lotto.utils;
 
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
 public class RandomGenerator {
+
+    private static final int MIN_RANDOM_NUM = 1;
+    private static final int MAX_RANDOM_NUM = 45;
+    private static final int RANDOM_NUM_COUNT = 6;
+
+    private RandomGenerator () {
+    }
+
+    public static List<Integer> generateRandomNum() {
+        return List.copyOf(pickUniqueNumbersInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM, RANDOM_NUM_COUNT));
+    }
 }
