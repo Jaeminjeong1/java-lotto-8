11b387033fda2540caaa5e76e120aa137b510431
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-01 21:37:40 +0900

    feat(ErrorMessage): 숫자 형식 에러메세지 구현

--- src/main/java/lotto/utils/ErrorMessage.java
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -2,7 +2,8 @@ package lotto.utils;
 
 public enum ErrorMessage {
 
-    EMPTY_INPUT_ERROR("입력이 비어있습니다. 다시 입력하세요.");
+    EMPTY_INPUT_ERROR("입력이 비어있습니다. 다시 입력하세요."),
+    NUMBER_FORMAT_ERROR("정수만 입력 가능합니다. 다시 입력하세요.");
 
 
     private final static String PREFIX = "[ERROR] ";
