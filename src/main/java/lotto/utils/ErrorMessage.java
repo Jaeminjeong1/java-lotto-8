9232d97eaff4a76392673756f1221cd85c19fa10
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-01 21:26:40 +0900

    feat(ErrorMessage): 빈 입력값 에러메세지 구현

--- src/main/java/lotto/utils/ErrorMessage.java
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -1,4 +1,18 @@
 package lotto.utils;
 
-public class ErrorMessage {
+public enum ErrorMessage {
+
+    EMPTY_INPUT_ERROR("입력이 비어있습니다. 다시 입력하세요.");
+
+
+    private final static String PREFIX = "[ERROR] ";
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = PREFIX + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
