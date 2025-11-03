22406ab73786414b642d6f595554f364e94ae2a9
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-03 15:38:39 +0900

    test(LottoTest): 로또 번호 개수 검증 테스트 코드 작성

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static lotto.utils.ErrorMessage.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTest {
+    @Test
+    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+        assertThatThrownBy(() -> Lotto.from(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 6개가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("provideInvalidLottoNumbers")
+    void 로또_번호가_6개가_아니면_예외가_발생한다(List<Integer> numbers) {
+        assertThatThrownBy(() -> Lotto.from(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_NUM_COUNT_ERROR.getMessage());
+    }
+
+    private static Stream<List<Integer>> provideInvalidLottoNumbers() {
+        return Stream.of(
+                List.of(1, 2, 3, 4, 5),
+                List.of(1, 2, 3, 4, 5, 6, 7)
+        );
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> Lotto.from(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
