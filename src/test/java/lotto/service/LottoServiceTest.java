6a25d9bbc566ab9714fa84770e4fff33b2ee43b3
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-03 16:22:42 +0900

    test(LottoService): 로또 장수 테스트 코드 구현

---
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -0,0 +1,32 @@
+package lotto.service;
+
+import lotto.domain.Result;
+import lotto.dto.LottoDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static lotto.utils.ErrorMessage.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoServiceTest {
+
+    LottoService lottoService = new LottoService();
+
+    @DisplayName("구매금액/1000 = 생성 장수")
+    @ParameterizedTest
+    @ValueSource(longs = {1000, 3000, 15000})
+    void 로또_장수_테스트(long userPrice) {
+        List<LottoDto> lottos = lottoService.generateLottos(userPrice);
+
+        assertThat(lottos).hasSize((int) (userPrice / 1000));
+    }
+
+}
