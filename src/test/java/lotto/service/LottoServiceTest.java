2d8ada8ba07f4713e8a60d4f36f5bf913a38ffb1
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-03 16:24:45 +0900

    test(LottoService): 파싱된 인자 가지고 lotto 잘 만드는지 테스트코드 구현

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -29,4 +29,13 @@ public class LottoServiceTest {
         assertThat(lottos).hasSize((int) (userPrice / 1000));
     }
 
+    @Test
+    @DisplayName("trim된 정수로 잘 파싱하여 LottoDto로 반환")
+    void 파싱_테스트() {
+        String[] parsed = {" 1", "2 ", " 3 ", "4", "5", "6"};
+        LottoDto winner = lottoService.generateWinnerLotto(parsed);
+
+        assertThat(winner.numbers()).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
 }
