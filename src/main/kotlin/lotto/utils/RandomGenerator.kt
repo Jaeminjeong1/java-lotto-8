981c5351bf8e1ff3e1b50eb0749a19b07816be36
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-07 23:49:40 +0900

    refactor(RandomGenerator): 클래스 전체 Kotlin으로 리펙터링

---
+++ src/main/kotlin/lotto/utils/RandomGenerator.kt
@@ -0,0 +1,14 @@
+package lotto.utils
+
+import camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange
+
+object RandomGenerator {
+
+    private const val MIN_RANDOM_NUM: Int = 1;
+    private const val MAX_RANDOM_NUM: Int = 45;
+    private const val RANDOM_NUM_COUNT: Int = 6;
+
+    @JvmStatic
+    fun generateRandomNum(): List<Int> =
+        pickUniqueNumbersInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM, RANDOM_NUM_COUNT).toList()
+}
