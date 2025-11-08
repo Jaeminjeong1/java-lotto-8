214b2f9e70aaca8edd9f86270a89310ab1a46375
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-08 16:51:01 +0900

    refactor(PurchaseAmount): 클래스 전체 Kotlin으로 리펙터링

---
+++ src/main/kotlin/lotto/domain/PurchaseAmount.kt
@@ -0,0 +1,34 @@
+package lotto.domain
+
+import lotto.utils.ErrorMessage.*
+
+data class PurchaseAmount private constructor(
+    val userPrice: Long
+) {
+
+    init {
+        validateMinimumPrice(userPrice)
+        validatePriceUnit(userPrice)
+    }
+
+    companion object {
+        const val LOTTO_PRICE: Int = 1000
+
+        @JvmStatic
+        fun from(userPrice: Long): PurchaseAmount = PurchaseAmount(userPrice)
+
+        private fun validateMinimumPrice(userPrice: Long) {
+            if (userPrice < LOTTO_PRICE) {
+                throw IllegalArgumentException(PRICE_LACK_ERROR.getMessage())
+            }
+        }
+
+        private fun validatePriceUnit(userPrice: Long) {
+            if (userPrice % LOTTO_PRICE != 0L) {
+                throw IllegalArgumentException(PRICE_UNIT_ERROR.getMessage())
+            }
+        }
+    }
+
+    fun calculateLottoCount(): Long = userPrice / LOTTO_PRICE
+}
