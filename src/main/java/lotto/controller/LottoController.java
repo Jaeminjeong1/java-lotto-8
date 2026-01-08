12fbb647ee1d4371996daabc4fa9311572009078
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-01-09 08:40:40 +0900

    feat: 초기 Application 구조 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,25 @@
+package lotto.controller;
+
+import lotto.service.LottoService;
+
+public class LottoController {
+
+    private final LottoService lottoService;
+
+    public LottoController(LottoService lottoService) {
+        this.lottoService = lottoService;
+    }
+
+    public void start() {
+        // 구입금액을 입력받는다
+
+        // 로또를 생성하여 출력한다.
+
+        // 당첨 번호를 입력받는다.
+
+        // 보너스 번호를 입력받는다.
+
+        // 당첨 통계를 낸 후 출력한다.
+
+    }
+}
